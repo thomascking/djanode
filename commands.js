14b21f1ccd38b0df62a77d31047b316480076d05
Thomas King <king.thomas.c@gmail.com>
2018-10-02 13:06:09 -0400

    added description to the createproject command

--- commands.js
+++ commands.js
@@ -6,27 +6,30 @@ var ncp = require('ncp').ncp;
 
 program.version('0.1.0');
 
-program.command('createproject [directory]').action((directory, command) => {
-  if (directory) {
-    console.log('Creating new project in', directory);
-  }
-  else {
-    console.log('Creating new project in current directory.');
-    directory = '.';
-  }
-  var sourceFolder = path.join(__dirname, 'template');
-  var destinationFolder = path.join(process.cwd(), directory);
-  fs.access(destinationFolder, fs.constants.F_OK, (err) => {
-    if (err) {
-      console.log('creating new directory for project');
-      fs.mkdir(destinationFolder, () => {
-        ncp(sourceFolder, destinationFolder, (err) => console.log(err ? err : 'copied template'));
-      });
+program
+  .command('createproject [directory]')
+  .description('Create a new project in the specified directory.')
+  .action((directory, command) => {
+    if (directory) {
+      console.log('Creating new project in', directory);
     }
     else {
-      ncp(sourceFolder, destinationFolder, (err) => console.log(err ? err : 'copied template'));
+      console.log('Creating new project in current directory.');
+      directory = '.';
     }
+    var sourceFolder = path.join(__dirname, 'template');
+    var destinationFolder = path.join(process.cwd(), directory);
+    fs.access(destinationFolder, fs.constants.F_OK, (err) => {
+      if (err) {
+        console.log('creating new directory for project');
+        fs.mkdir(destinationFolder, () => {
+          ncp(sourceFolder, destinationFolder, (err) => console.log(err ? err : 'copied template'));
+        });
+      }
+      else {
+        ncp(sourceFolder, destinationFolder, (err) => console.log(err ? err : 'copied template'));
+      }
+    });
   });
-});
 
 program.parse(process.argv);
